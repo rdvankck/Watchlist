d1459b98aac7445d87b1cf6c39c4af85defd9552
rdvankck <kck.rdvan@gmail.com>
2026-01-07 17:46:45 +0300

    feat: modernize Search page

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/5001/api';
+const API_URL = 'http://localhost:5001/api';
 
 export const register = async (userData) => {
     const response = await axios.post(`${API_URL}/auth/register`, userData);
